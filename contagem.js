656f3a35922bf41dc205fbd8d094b8baaf8389d4
Wilson-Carvalho <wilson.carvalho2543@gmail.com>
2022-02-26 19:06:36 -0300

    Sistema de contagem finalizado

---
+++ contagem.js
@@ -0,0 +1,16 @@
+let numeroAlunos = 10;
+
+for(let contador = 0;contador < numeroAlunos; contador++) {
+        // Resto % -> retorna o resto de  uma divisão inteira
+
+    if(contador == 0) { 
+        console.log("O numero é ZERO")
+    } else if(contador % 2 == 0) {
+        // concatenação
+        console.log(`O numero` + contador + ` é PAR`)
+    } else if(contador % 2  == 1) {
+        // Interpolação
+        console.log(`O numero ${contador} é IMPAR`)
+    }
+}
+
